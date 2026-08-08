ae50486c5b1f0b6baf46755f97dfb9fc1e0cb868
ishaan <ishaan>
2026-08-08 15:16:31 +0530

    cleaning up the directory

---
+++ hello/hello.c
@@ -0,0 +1,25 @@
+#include<stdio.c>
+
+
+char* init(){
+    char* str = "\nThis is a sample Test Program, for Ishaan Pandey\nyour task is to complete the area program to find the area of a circle, the `area` function\nyou can check the help.txt foe help\n\nGood Luck Kiddo :) <3\nUwU\nOwO\n";
+    return str;
+}
+
+float area(int r){
+    //compete this program to calculate the area of a circle
+    float area=(float)r*r*(3.14);
+    return area;
+}
+
+int main(){
+    char* str = init();
+
+    int r;
+    
+    printf(str);
+    printf("Enter the radius of the circle: ");
+    scanf("%d", &r);
+    float result = area(r);
+    printf("The area of the circle is: %f\n", result);
+}
