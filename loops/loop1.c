ad770a00ce9fd4f14f1be21e1f3aa82297ac611f
Taran-0107 <132281589+Taran-0107@users.noreply.github.com>
2026-08-11 03:02:48 +0530

    return function

--- loops/loop1.c
+++ loops/loop1.c
@@ -2,15 +2,53 @@
 
 //dekh lo ab
 
-int loop(int a){
+void forloop(int a){
+
+    printf("For Loop\n");
 
     for (int i=0;i<a;i++){
         printf("%d\n",i);
+    }    
+
+}
+
+void whileloop(int a){
+
+    printf("While Loop\n");
+
+    int i=0;
+    while (i<a){
+        printf("%d\n",i);
+        i++;
     }
 }
+
+void loop(int a, int type){
+
+    if(type==1){
+        forloop(a);
+    }
+    else{
+        whileloop(a);
+    }
+
+}
+
+int min(int a,int b){
+    if(a<b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
 int main(){
 
-    loop(10);
+    loop(10,0);
+    loop(10,1);
+
+    printf("sum is %d\n",min(10,20));
 
     return 0;
 }
